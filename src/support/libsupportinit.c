5588feea875d9c0580acb4a6a4a21d8b9b7e7a67
Jameson Nash <vtjnash@gmail.com>
2020-10-07 12:35:08 -0400

    ensure libc is using a UTF8-compatible encoding
    Some basic functionality in libc (such as printf) might be broken if the
    string encoding is not matching our Cstring expectations.

--- src/support/libsupportinit.c
+++ src/support/libsupportinit.c
@@ -7,18 +7,27 @@
 extern "C" {
 #endif
 
-static int isInitialized = 0;
-
 void libsupport_init(void)
 {
+    static int isInitialized = 0;
     if (!isInitialized) {
-
-        setlocale(LC_ALL, ""); // set to user locale
-        setlocale(LC_NUMERIC, "C"); // use locale-independent numeric formats
-
         ios_init_stdstreams();
 
-        isInitialized=1;
+        // adopt the user's locale for most formatting
+        setlocale(LC_ALL, "");
+        // but use locale-independent numeric formats (for parsing)
+        setlocale(LC_NUMERIC, "C");
+        // and try to specify ASCII or UTF-8 (preferred) for our Libc and Cstring functions
+        setlocale(LC_CTYPE, "C"); // ASCII
+#ifndef _OS_WINDOWS_
+        if (setlocale(LC_CTYPE, "C.UTF-8") == NULL) { // Linux name
+            if (setlocale(LC_CTYPE, "UTF-8") == NULL) { // BSD name
+                ios_puts("WARNING: failed to select UTF-8 encoding, using ASCII\n", ios_stderr);
+            }
+        }
+#endif
+
+        isInitialized = 1;
     }
 }
 
